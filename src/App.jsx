28936eda8501509fe64d639036c8afa09fa5d33a
Sunny Kumar <sk202542@gmail.com>
2025-05-20 00:23:59 +0530

    feat: enhance registration and login functionality; add user input handling and form submission

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,23 @@ import UserLayout from './components/Layout/UserLayout'
 import Home from './pages/Home'
 import { Toaster } from 'sonner'
 import Login from './pages/Login'
+import Register from './pages/Register'
 
 function App() {
   return (
-  <BrowserRouter>
-  <Toaster position='top-right'/>
-  <Routes>
-    <Route path='/' element={<UserLayout/>}>
-    {/*User Layout*/}
-    <Route index element={<Home/>}/>
-    <Route path='login' element={<Login/>} />
-    </Route>
-    <Route>{/*Admin Layout*/}</Route>
+    <BrowserRouter>
+      <Toaster position='top-right' />
+      <Routes>
+        <Route path='/' element={<UserLayout />}>
+          {/*User Layout*/}
+          <Route index element={<Home />} />
+          <Route path='login' element={<Login />} />
+          <Route path='register' element={<Register />} />
+
+        </Route>
+        <Route>{/*Admin Layout*/}</Route>
       </Routes>
-  </BrowserRouter>
+    </BrowserRouter>
   )
 }
 
